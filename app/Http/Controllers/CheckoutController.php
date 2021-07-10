a32d86404a3522810788703e992227175263929e
Ashraf Kabir <ashrafkabir95@gmail.com>
2021-07-10 16:54:27 +0600

    Trait->StripeTrait added, transaction query added

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Cart;
+use App\Http\Traits\StripeTrait;
 use App\Models\Order;
 use App\Models\Transactions;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
 
-// use Stripe\Charge;
-// use Stripe\Stripe;
-
 class CheckoutController extends Controller
 {
+  use StripeTrait;
+
   public function checkout()
   {
     if (!Session::has('client'))
@@ -63,113 +64,71 @@ class CheckoutController extends Controller
       'cvc'        => 'required'
     ]);
 
-    $total_price = (float) $cart->total_price;
+    $total_price       = (float) $cart->total_price;
+    $stripe_card_token = $request->input('stripe_token');
 
     // var_dump($total_price);
     // die();
 
-    $stripe_secret_key = env('STRIPE_SECRET_KEY');
+    $response = $this->stripe_charge($total_price, $stripe_card_token);
 
-    $stripe = new \Stripe\StripeClient(
-      $stripe_secret_key
-    );
+    if (isset($response['success']))
+    {
+      DB::beginTransaction();
 
-    try {
-      $charge = $stripe->charges->create([
-        'amount'      => $total_price * 100,
-        'currency'    => 'usd',
-        'source'      => $request->input('stripe_token'), // generated from checkout.js
-        'description' => 'Test Charge'
-      ]);
+      $order = new Order();
 
-      if (isset($charge['id']))
+      $order->name          = $request->input('first_name') . ' ' . $request->input('last_name');
+      $order->email         = $request->input('email');
+      $order->phone         = $request->input('phone');
+      $order->address       = $request->input('address_1') . ' ' . $request->input('address_2');
+      $order->city          = $request->input('city');
+      $order->state         = $request->input('state');
+      $order->zip           = $request->input('zip');
+      $order->country       = $request->input('country');
+      $order->cart          = serialize($cart);
+      $order->payment_id    = $response['charge']->id;
+      $order->checkout_type = 1; // 1 -> self
+      $order->user_id       = $user->id;
+
+      $check_order_save = $order->save();
+
+      if ($check_order_save)
       {
-        $order = new Order();
-
-        $order->name          = $request->input('first_name') . ' ' . $request->input('last_name');
-        $order->email         = $request->input('email');
-        $order->phone         = $request->input('phone');
-        $order->address       = $request->input('address_1') . ' ' . $request->input('address_2');
-        $order->city          = $request->input('city');
-        $order->state         = $request->input('state');
-        $order->zip           = $request->input('zip');
-        $order->country       = $request->input('country');
-        $order->cart          = serialize($cart);
-        $order->payment_id    = $charge['id'];
-        $order->checkout_type = 1;       // 1 -> self
-        $order->user_id       = $user->id;
-
-        $order->save();
-
         $transaction = new Transactions();
 
         $transaction->subtotal     = $total_price;
         $transaction->tax          = 0;
         $transaction->discount     = 0;
         $transaction->total        = $total_price;
-        $transaction->payment_id   = $charge['id'];
-        $transaction->payment_type = 1;       // 1 -> card, 2 -> cash
+        $transaction->payment_id   = $response['charge']->id;
+        $transaction->payment_type = 1; // 1 -> card, 2 -> cash
         $transaction->user_id      = $user->id;
 
-        $transaction->save();
+        $check_transaction_save = $transaction->save();
+
+        if ($check_transaction_save)
+        {
+          DB::commit();
+          Session::forget('cart');
+          return redirect('cart')->with('success', 'Payment successful.');
+        }
+        else
+        {
+          DB::rollback();
+          return redirect('checkout')->with('error', 'Payment failed.');
+        }
       }
       else
       {
+        DB::rollback();
         return redirect('checkout')->with('error', 'Payment failed.');
       }
     }
-    catch (\Stripe\Exception\CardException $e)
+    else
     {
-      Session::put('error', $e->getMessage());
+      Session::put('error', $response['error_msg']);
       return redirect::to('/checkout');
     }
-    catch (\Stripe\Exception\RateLimitException $e)
-    {
-      // Too many requests made to the API too quickly
-      // echo 'Message is:' . $e->getError()->message . '\n';
-
-      Session::put('error', $e->getMessage());
-      return redirect::to('/checkout');
-    }
-    catch (\Stripe\Exception\InvalidRequestException $e)
-    {
-      // Invalid parameters were supplied to Stripe's API
-      // echo 'Message is:' . $e->getError()->message . '\n';
-
-      Session::put('error', $e->getMessage());
-      return redirect::to('/checkout');
-    }
-    catch (\Stripe\Exception\AuthenticationException $e)
-    {
-      // Authentication with Stripe's API failed
-      // (maybe you changed API keys recently)
-      Session::put('error', $e->getMessage());
-      return redirect::to('/checkout');
-    }
-    catch (\Stripe\Exception\ApiConnectionException $e)
-    {
-      // Network communication with Stripe failed
-      Session::put('error', $e->getMessage());
-      return redirect::to('/checkout');
-    }
-    catch (\Stripe\Exception\ApiErrorException $e)
-    {
-      // Display a very generic error to the user, and maybe send
-      // yourself an email
-      Session::put('error', $e->getMessage());
-      return redirect::to('/checkout');
-    }
-    catch (\Exception $e)
-    {
-      // echo '<pre>';
-      // print_r($e);
-      // die();
-      Session::put('error', $e->getMessage());
-      return redirect::to('/checkout');
-    }
-
-    Session::forget('cart');
-
-    return redirect('cart')->with('success', 'Payment successful.');
   }
 }
---
+++ app/Http/Traits/StripeTrait.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Traits;
+
+trait StripeTrait
+{
+  public function stripe_charge($total_price, $stripe_card_token)
+  {
+    $stripe_secret_key = env('STRIPE_SECRET_KEY');
+
+    $stripe = new \Stripe\StripeClient(
+      $stripe_secret_key
+    );
+
+    if (!empty($total_price) && !empty($stripe_card_token))
+    {
+      try {
+        $charge = $stripe->charges->create([
+          'amount'      => $total_price * 100,
+          'currency'    => 'usd',
+          'source'      => $stripe_card_token,
+          'description' => 'Test Product Charge'
+        ]);
+
+        $output['success'] = TRUE;
+        $output['charge']  = $charge;
+        return $output;
+        exit();
+      }
+      catch (\Stripe\Exception\CardException $e)
+      {
+        $output['error']     = TRUE;
+        $output['error_msg'] = $e->getMessage();
+        return $output;
+        exit();
+      }
+      catch (\Stripe\Exception\RateLimitException $e)
+      {
+        // Too many requests made to the API too quickly
+        // echo 'Message is:' . $e->getError()->message . '\n';
+
+        $output['error']     = TRUE;
+        $output['error_msg'] = $e->getMessage();
+        return $output;
+        exit();
+      }
+      catch (\Stripe\Exception\InvalidRequestException $e)
+      {
+        // Invalid parameters were supplied to Stripe's API
+        // echo 'Message is:' . $e->getError()->message . '\n';
+
+        $output['error']     = TRUE;
+        $output['error_msg'] = $e->getMessage();
+        return $output;
+        exit();
+      }
+      catch (\Stripe\Exception\AuthenticationException $e)
+      {
+        // Authentication with Stripe's API failed
+        // (maybe you changed API keys recently)
+        $output['error']     = TRUE;
+        $output['error_msg'] = $e->getMessage();
+        return $output;
+        exit();
+      }
+      catch (\Stripe\Exception\ApiConnectionException $e)
+      {
+        // Network communication with Stripe failed
+        $output['error']     = TRUE;
+        $output['error_msg'] = $e->getMessage();
+        return $output;
+        exit();
+      }
+      catch (\Stripe\Exception\ApiErrorException $e)
+      {
+        // Display a very generic error to the user, and maybe send
+        // yourself an email
+        $output['error']     = TRUE;
+        $output['error_msg'] = $e->getMessage();
+        return $output;
+        exit();
+      }
+      catch (\Exception $e)
+      {
+        $output['error']     = TRUE;
+        $output['error_msg'] = $e->getMessage();
+        return $output;
+        exit();
+      }
+    }
+    else
+    {
+      $output['error']     = TRUE;
+      $output['error_msg'] = "Error! Please fill data correctly.";
+      return $output;
+      exit();
+    }
+  }
+}
